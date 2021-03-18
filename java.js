80ac66dd75a35878fc0c3a704d0dbf0ad230e65f
Alia999 <aliaahmad1998a@gmail.com>
2021-03-18 19:12:06 +0200

    java

---
+++ java.js
@@ -0,0 +1,9 @@
+let name=prompt("welcome to our website. whats your name? ");
+alert ('Hello '+ name)
+let answer=prompt("Do you want to contact us?")
+alert ("this is our phone number:07777777777")
+ console.log("you are here to develop your personality")
+ 
+   
+
+
