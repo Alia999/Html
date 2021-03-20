539f950bfd3217df8862c1db80e491f5d00634ec
Alia999 <aliaahmad1998a@gmail.com>
2021-03-20 14:37:44 +0200

    it was not working

--- java.js
+++ java.js
@@ -1,8 +1,7 @@
 console.log("You are here to develop your personality")
-
+let name =prompt('Welcome to our website.Whats your name');
+alert('Helo '+ name);
 var YourAge = prompt('Whats your age?')
 if (YourAge>18)  {alert('welcome')  }
 else if(YourAge<18) {alert('go home')  }
 else  {alert('welcome ')  }
-let name =prompt('Welcome to our website.Whats your name');
-alert('Helo '+ name)
