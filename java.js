81d1bf519dde7d8eebbd211d0911c403efaec3fd
Alia999 <aliaahmad1998a@gmail.com>
2021-03-20 18:18:41 +0200

    to make sence

--- java.js
+++ java.js
@@ -2,6 +2,5 @@ console.log("You are here to develop your personality")
 let name =prompt('Welcome to our website.Whats your name');
 alert('Helo '+ name);
 var YourAge = prompt('Whats your age?')
-if (YourAge>18)  {alert('welcome')  }
-else if(YourAge<18) {alert('go home')  }
-else  {alert('welcome ')  }
+if (YourAge>=18)  {alert('We have the best psychologists for adults')  }
+else  {alert('At the bottom of the screen you will find a link, please use it ')  }
